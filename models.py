12f2fdf91dd05959ac817fe5dc1ef0bf096969e0
Tcbowen <tomb3326@gmail.com>
2021-03-27 16:06:07 -1000

    added html template for Iq test

--- models.py
+++ models.py
@@ -1,13 +1,9 @@
 from otree.api import (
-    models,
-    widgets,
-    BaseConstants,
-    BaseSubsession,
-    BaseGroup,
-    BasePlayer,
-    Currency as c,
-    currency_range,
+    models, BaseConstants
 )
+from otree_markets import models as markets_models
+from otree_markets.exchange.base import Order
+from .configmanager import ConfigManager
 
 
 author = 'Your name here'
@@ -18,18 +14,33 @@ Your app description
 
 
 class Constants(BaseConstants):
-    name_in_url = 'iq_test'
+    name_in_url = 'iq_test' 
     players_per_group = None
     num_rounds = 1
 
+    config_fields = {
+            'test_number': int,
+            'answers_1': int , 
+            'answers_2': int,
+            'answers_3': int, 
+            'answers_4': int 
+     }
+class Subsession(markets_models.Subsession):
+    def config(self):
+        config_addr = Constants.name_in_url + '/configs/' + self.session.config['config_file']
+        return ConfigManager(config_addr, self.round_number, Constants.config_fields)
 
-class Subsession(BaseSubsession):
+    def creating_session(self):
+        for player in self.get_players():
+            player.test_number = self.config.test_number
+
+class Group(markets_models.Group):
     pass
 
 
-class Group(BaseGroup):
-    pass
-
-
-class Player(BasePlayer):
-    pass
+class Player(markets_models.Player):
+    
+    test_number = models.IntegerField()
+    Question_1 = models.IntegerField(
+        choices=[1,2,3,4,5,6,7,8],
+                )
--- pages.py
+++ pages.py
@@ -3,16 +3,87 @@ from ._builtin import Page, WaitPage
 from .models import Constants
 
 
-class MyPage(Page):
-    pass
+class Question_1(Page):
+  
+	def vars_for_template(self):
+	    Q_url = '/static/iq_test/sample_{{}}/sample_{{}}/Q_1.png'.format(self.player.test_number)
 
+	    return {
+	      	'Q': Q_url
+	      }
+	form_model = 'player'
+	form_fields = ['Question_1']
 
-class ResultsWaitPage(WaitPage):
-    pass
+class Question_2(Page):
+    def vars_for_template(self):
+	    Q_url = '/static/iq_test/sample_1/sample_1/Q_2.png'
 
+	    return {
+	      	'Q': Q_url
+	      }
 
-class Results(Page):
-    pass
+class Question_3(Page):
+    def vars_for_template(self):
+	    Q_url = '/static/iq_test/sample_1/sample_1/Q_3.png'
 
+	    return {
+	      	'Q': Q_url
+	      }
 
-page_sequence = [MyPage, ResultsWaitPage, Results]
+class Question_4(Page):
+    def vars_for_template(self):
+	    Q_url = '/static/iq_test/sample_1/sample_1/Q_4.png'
+
+	    return {
+	      	'Q': Q_url
+	      }
+
+class Question_5(Page):
+    def vars_for_template(self):
+	    Q_url = '/static/iq_test/sample_1/sample_1/Q_5.png'
+
+	    return {
+	      	'Q': Q_url
+	      }
+
+class Question_6(Page):
+    def vars_for_template(self):
+	    Q_url = '/static/iq_test/sample_1/sample_1/Q_6.png'
+
+	    return {
+	      	'Q': Q_url
+	      }
+
+class Question_7(Page):
+    def vars_for_template(self):
+	    Q_url = '/static/iq_test/sample_1/sample_1/Q_7.png'
+
+	    return {
+	      	'Q': Q_url
+	      }
+
+class Question_8(Page):
+    def vars_for_template(self):
+	    Q_url = '/static/iq_test/sample_1/sample_1/Q_8.png'
+
+	    return {
+	      	'Q': Q_url
+	      }
+
+class Question_9(Page):
+    def vars_for_template(self):
+	    Q_url = '/static/iq_test/sample_1/sample_1/Q_9.png'
+
+	    return {
+	      	'Q': Q_url
+	      }
+
+class Question_10(Page):
+    def vars_for_template(self):
+	    Q_url = '/static/iq_test/sample_1/sample_1/Q_10.png'
+
+	    return {
+	      	'Q': Q_url
+	      }
+
+page_sequence = [Question_1]
